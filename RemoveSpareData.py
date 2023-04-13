978eeb8a5ee750cde8cd144405e7280a3faebc46
CertAndKey <38504609+CertAndKey@users.noreply.github.com>
2023-04-13 19:28:57 +0000

    Update and rename clip.py to RemoveSpareData.py

--- RemoveSpareData.py
+++ RemoveSpareData.py
@@ -1,3 +1,6 @@
+#ARGUMENT IS .BIN FILE TO BE PARSED
+#PRINTS GOOD DATA INTO PARSED.BIN
+#PRINTS BAD DATA INTO JUNK.BIN 
 import sys
 
 input_file = open(sys.argv[1], 'rb')
@@ -5,10 +8,10 @@ output_file = open("parsed.bin", "wb")
 junk_file = open("junk.bin", "wb")
 
 i=0
-while(i < 131072):
+while(i < <number_of_pages>):
     try:
-        output_file.write(input_file.read(2048))
-        junk_file.write(input_file.read(128))
+        output_file.write(input_file.read(<data_size>))
+        junk_file.write(input_file.read(<spare_data_size>))
         i += 1
     except:
         output_file.close()
